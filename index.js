057824c5b0b101987e14a18e5a666b6348070bdd
github username <github email address>
2019-08-22 11:44:05 -0500

    Q2 dog App

--- index.js
+++ index.js
@@ -3,19 +3,29 @@
 function getDogImages(amount) {
   fetch(`https://dog.ceo/api/breeds/image/random/${amount}`)
     .then(response => response.json())
-    .then(responseData => extractData(responseData, amount))
+    .then(responseData => {
+      const extractedImages = extractData(responseData, amount);
+      displayOnDom(extractedImages);
+    })
     .catch(error => console.log(error));
 }
 
 function extractData(jsonData, amount) {
+  const allImages = [];
   for(let i = 0; i < amount; i++) {
     let imageURL = jsonData.message[i];
+    allImages.push(imageURL);
     console.log(imageURL);
   }
+  return allImages; 
 }
 
-function appendToDOM() {
-
+function displayOnDom(imageArr) {
+  let imgString = ``;
+  for(let i = 0; i < imageArr.length; i++){
+    imgString += `<img src="${imageArr[i]}">`
+  }
+  $('.js-display-images').html(imgString);
 }
 
 function handleSubmitClicked() {
